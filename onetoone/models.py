8b136b7fb9e245aaf262c57113926f69d776ed7e
Alisher <117917625+tilyavov@users.noreply.github.com>
2026-01-22 15:53:50 +0500

    onetoone-onetomany

--- onetoone/models.py
+++ onetoone/models.py
@@ -7,4 +7,5 @@ class Account(models.Model):
     login = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
     
